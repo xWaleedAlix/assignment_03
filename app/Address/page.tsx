c862078244441c92e9ea6b98c1bcbbbce72b1f06
Waleed-Ali <waleedali534363279@gmail.com>
2024-09-01 13:50:37 +0500

    update

--- app/Address/page.tsx
+++ app/Address/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const page = () => {
   return (
     <div>
-      <Image src="/adress.jpg" alt='adress-pic' width="500" height="500"  className='house'></Image>
+      <Image src="/Adress.jpg" alt='adress-pic' width="500" height="500"  className='house'></Image>
       <p>Thali Chock Bypass Road <br></br> <b> Rahim Yar Khan</b></p>
     </div>
   )
